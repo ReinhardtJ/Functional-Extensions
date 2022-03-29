65f8bf3051ed718a26ba5cabdb292edf9b148f3e
ReinhardtJ <ersatz.jonas@yahoo.de>
2022-03-29 21:32:01 +0200

    added new version number for version 0.2.0

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('README.md', 'r') as f:
       long_description = f.read()
 
 setup(name='funext',
-      version='0.1.1',
+      version='0.2.0',
       author='Jonas Reinhardt',
       author_email='jonas@reinhardt.ai',
       description='Functional extensions for Python objects',
